0342ee9ca3f5f5731a4d58bea852bb1d68ed84cb
cponeill <caseyoneill78@hotmail.com>
2016-11-03 10:39:12 -0400

    adding simple_match.py

---
+++ re/simple_match.py
@@ -0,0 +1,13 @@
+# simple_match.py
+
+import re
+
+pattern = 'this'
+text = 'Does this text match the pattern?'
+
+match = re.search(pattern, text)
+
+s = match.start()
+e = match.end()
+
+print('Found "{}"\nin "{}"\nfrom {} to {} ("{}")'.format(match.re.pattern, match.string, s, e, text[s:e]))
