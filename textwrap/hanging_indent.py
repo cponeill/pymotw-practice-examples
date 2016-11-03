707c4f86b171f7a118ab47c5f82b5c5867c186a3
cponeill <caseyoneill78@hotmail.com>
2016-11-03 09:52:55 -0400

    adding hanging_indent.py

---
+++ textwrap/hanging_indent.py
@@ -0,0 +1,11 @@
+# hanging_indent.py
+
+import textwrap
+from textwrap_example import sample_text
+
+dedented_text = textwrap.dedent(sample_text).strip()
+print(textwrap.fill(dedented_text,
+                    initial_indent='',
+                    subsequent_indent=' ' * 4,
+                    width=50,
+                    ))
