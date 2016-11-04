b1ca6b010dfab793c5e097a30591333232316b19
cponeill <caseyoneill78@hotmail.com>
2016-11-04 10:43:52 -0400

    adding finditer.py

---
+++ re/finditer.py
@@ -0,0 +1,12 @@
+# finditer.py
+
+import re
+
+text = 'abbaaabbbbaaaaa'
+
+pattern = 'ab'
+
+for match in re.finditer(pattern, text):
+    s = match.start()
+    e = match.end()
+    print('Found {!r} at {:d}:{:d}'.format(text[s:e], s, e))
