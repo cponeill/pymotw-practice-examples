179c366deaf682e9715f178a5c98c2498caa3ff7
cponeill <caseyoneill78@hotmail.com>
2016-11-05 11:50:14 -0400

    adding charset.py

---
+++ re/charset.py
@@ -0,0 +1,10 @@
+# charset.py
+
+from test_patterns import test_patterns
+
+test_patterns(
+    'abbaabbba',
+    [('[ab]', 'either a or b'),
+     ('a[ab]+', 'a followed by 1 or more a or b'),
+     ('a[ab]+?', 'a followed by 1 or more a or b, not greedy')],
+)
