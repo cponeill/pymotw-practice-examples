dec8c7a6efb62cf45fd9e11b5f0c4515bfc829e3
cponeill <caseyoneill78@hotmail.com>
2016-11-05 11:51:09 -0400

    adding exclude to charset.py

--- re/charset.py
+++ re/charset.py
@@ -8,3 +8,10 @@ test_patterns(
      ('a[ab]+', 'a followed by 1 or more a or b'),
      ('a[ab]+?', 'a followed by 1 or more a or b, not greedy')],
 )
+
+
+
+test_patterns(
+    'This is some text -- with punctuation.',
+    [('[^-. ]+', 'sequences without -, ., or space')],
+)
