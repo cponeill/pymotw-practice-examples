996f5094749f982c6a5b0e5914d7d49ba3bbd0fc
cponeill <caseyoneill78@hotmail.com>
2016-11-07 12:24:16 -0500

    adding fullmatch.py

---
+++ re/fullmatch.py
@@ -0,0 +1,14 @@
+# fullmatch.py
+
+import re
+
+text = 'This is some text -- with punctuation.'
+pattern = 'is'
+
+print('Text       :', text)
+print('Pattern    :', pattern)
+
+m = re.search(pattern, text)
+print('Search     :', m)
+s = re.fullmatch(pattern, text)
+print('Full match :', s)
