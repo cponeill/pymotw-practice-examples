98b18cf7eeaeacb536ba05dbe998e45107c89407
cponeill <caseyoneill78@hotmail.com>
2016-11-07 12:23:01 -0500

    adding match.py

---
+++ re/match.py
@@ -0,0 +1,14 @@
+# match.py
+
+import re
+
+text = 'This is some text -- with punctuation.'
+pattern = 'is'
+
+print('Text   :', text)
+print('Pattern:', pattern)
+
+m = re.match(pattern, text)
+print('Match  :', m)
+s = re.search(pattern, text)
+print('Search :', s)
