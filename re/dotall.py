b6eb4ecaf6c682df48174a98a10ee8c7922d96b4
cponeill <caseyoneill78@hotmail.com>
2016-11-11 08:02:29 -0500

    adding dotall.py

---
+++ re/dotall.py
@@ -0,0 +1,17 @@
+# dotall.py
+
+import re
+
+text = 'This is some text -- with puncutation.\nA second line.'
+pattern = r'.+'
+no_newlines = re.compile(pattern)
+dotall = re.compile(pattern, re.DOTALL)
+
+print('Text:\n  {!r}'.format(text))
+print('Pattern:\n  {}'.format(pattern))
+print('No newlines :')
+for match in no_newlines.findall(text):
+    print('  {!r}'.format(match))
+print('Dotall      :')
+for match in dotall.findall(text):
+    print('  {!r}'.format(match))
