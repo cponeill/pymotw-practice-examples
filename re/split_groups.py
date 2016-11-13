8e118f96f944753607e35cb1eca00cff32ea53cc
cponeill <caseyoneill78@hotmail.com>
2016-11-13 14:37:01 -0500

    adding split_groups.py

---
+++ re/split_groups.py
@@ -0,0 +1,16 @@
+# split_groups.py
+
+import re
+
+text = '''Paragraph one
+on two lines.
+
+Paragraph two.
+
+
+Paragraph three.'''
+
+print('With split:')
+for num, para in enumerate(re.split(r'(\n{2,})', text)):
+    print(num, repr(para))
+    print()
