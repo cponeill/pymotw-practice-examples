75ce72cf66126bba768f399891601bde8a17ff1a
cponeill <caseyoneill78@hotmail.com>
2016-11-20 10:07:44 -0500

    getservbyport.py

---
+++ socket/getservbyport.py
@@ -0,0 +1,8 @@
+# getservbyport.py
+
+import socket
+from urllib.parse import urlparse
+
+for port in [80, 443, 21, 70, 25, 143, 993, 110, 995]:
+    url = '{}://example.com/'.format(socket.getservbyport(port))
+    print(url)
