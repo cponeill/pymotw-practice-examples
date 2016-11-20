f8e3f9bb299fd5df1b0a16b9a9044daa546d0a4f
cponeill <caseyoneill78@hotmail.com>
2016-11-20 10:06:51 -0500

    address_packing.py

---
+++ socket/address_packing.py
@@ -0,0 +1,13 @@
+# address_packing.py
+
+import binascii
+import socket
+import struct
+import sys
+
+for string_address in ['192.168.1.1', '127.0.0.1']:
+    packed = socket.inet_aton(string_address)
+    print('Original:', string_address)
+    print('Packed  :', binascii.hexlify(packed))
+    print('Unpacked:', socket.inet_ntoa(packed))
+    print()
