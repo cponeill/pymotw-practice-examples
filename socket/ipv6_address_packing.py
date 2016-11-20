88ed96654c8215074bc06dca3144a170e9376540
cponeill <caseyoneill78@hotmail.com>
2016-11-20 10:07:56 -0500

    ipv6_address_packing.py

---
+++ socket/ipv6_address_packing.py
@@ -0,0 +1,13 @@
+# ipv5_address_packing.py
+
+import binascii
+import socket
+import struct
+import sys
+
+string_address = '2002:ac10:10a:1234:21e:52ff:fe74:40e'
+packed = socket.inet_pton(socket.AF_INET6, string_address)
+
+print('Original:', string_address)
+print('Packed  :', binascii.hexlify(packed))
+print('Unpacked:', socket.inet_ntop(socket.AF_INET6, packed))
