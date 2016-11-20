4e499c0a5528c5eb0ea91c4b54c273155b225638
cponeill <caseyoneill78@hotmail.com>
2016-11-20 10:07:19 -0500

    getaddrinfo.py

---
+++ socket/getaddrinfo.py
@@ -0,0 +1,28 @@
+# getaddrinfo.py
+
+import socket
+
+def get_constants(prefix):
+    """Create a dictionary mapping socket module
+       constants to their names."""
+    return {
+        getattr(socket, n): n
+        for n in dir(socket)
+        if n.startswith(prefix)
+    }
+
+families = get_constants('AF_')
+types = get_constants('SOCK_')
+protocols = get_constants('IPPROTO_')
+
+for response in socket.getaddrinfo('www.python.org', 'http'):
+    
+    # Unpack the response tuple
+    family, socktype, proto, canonname, sockaddr = response
+
+    print('Family        :', families[family])
+    print('Type          :', types[socktype])
+    print('Protocol      :', protocols[proto])
+    print('Canonical name:', canonname)
+    print('Socket address:', sockaddr)
+    print()
