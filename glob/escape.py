05daed2334339defceca62723fb38efbcee073cd
cponeill <caseyoneill78@hotmail.com>
2016-11-28 09:55:02 -0500

    escape.py

---
+++ glob/escape.py
@@ -0,0 +1,12 @@
+# escape.py
+
+import glob
+
+specials = '?*['
+
+for char in specials:
+    pattern = 'dir/*' + glob.escape(char) + '.txt'
+    print('Searching for: {!r}'.format(pattern))
+    for name in sorted(glob.glob(pattern)):
+        print(name)
+    print()
