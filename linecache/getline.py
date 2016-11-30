ad1c502a864439b9776ee5d5bbd6b83c2cf2a80f
cponeill <caseyoneill78@hotmail.com>
2016-11-30 10:16:38 -0500

    getline.py

---
+++ linecache/getline.py
@@ -0,0 +1,17 @@
+# getline.py
+
+import linecache
+from data import *
+
+filename = make_tempfile()
+
+# Pick out the same line from source and cache.
+# (Notice that linecache counts from 1)
+print('SOURCE:')
+print('{!r}'.format(lorem.split('\n')[4]))
+print()
+print('CACHE:')
+print('{!r}'.format(linecache.getline(filename, 5)))
+
+
+cleanup(filename)
