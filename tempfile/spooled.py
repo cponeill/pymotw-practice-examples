cd846c84fa99fdb6d7547c1092b6849298e07005
cponeill <caseyoneill78@hotmail.com>
2016-12-02 09:48:13 -0500

    spooled.py

---
+++ tempfile/spooled.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import tempfile
+
+import tempfile
+
+with tempfile.SpooledTemporaryFile(max_size=100,
+                                   mode='w+t',
+                                   encoding='utf-8') as temp:
+    print('temp: {!r}'.format(temp))
+
+    for i in range(3):
+        temp.write('This line is repeated over and over.\n')
+        print(temp._rolled, temp._file)
