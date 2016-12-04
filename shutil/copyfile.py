856ebaaa29b47f5370489e09cf91f22c540f2654
cponeill <caseyoneill78@hotmail.com>
2016-12-04 10:32:01 -0500

    copyfile.py

---
+++ shutil/copyfile.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import glob
+import shutil
+
+print('BEFORE:', glob.glob('shutil_copyfile.*'))
+
+shutil.copyfile('shutil_copyfile.py', 'shutil_copyfile.py.copy')
+
+print('AFTER:', glob.glob('shutil_copyfile.*'))
