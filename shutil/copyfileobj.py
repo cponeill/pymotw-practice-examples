05a9182f2287918efcd1cfe6c89269be17c5ab19
cponeill <caseyoneill78@hotmail.com>
2016-12-04 10:39:30 -0500

    copyfileobj.py

---
+++ shutil/copyfileobj.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import io
+import os
+import shutil
+import sys
+
+class VerboseStringIO(io.StringIO):
+
+    def read(self, n=1):
+        next = io.StringIO.read(self, n)
+        print('read({}) bytes'.format(n))
+        return next
+
+lorem_ipsum = '''Lorem ipsum dolor sit amet, consectetuer
+adipiscing elit.  Vestibulum aliquam mollis dolor. Donec
+vulputate nunc ut diam. Ut rutrum mi vel sem. Vestibulum
+ante ipsum.'''
+
+
+print('Default:')
+input = VerboseStringIO(lorem_ipsum)
+output = io.StringIO()
+shutil.copyfileobj(input, output)
+
+print()
+
+print('All at once')
+input = VerboseStringIO(lorem_ipsum)
+output = io.StringIO()
+shutil.copyfileobj(input, output, -1)
+
+print()
+
+
+print('Blocks of 256:')
+input = VerboseStringIO(lorem_ipsum)
+output = io.StringIO()
+shutil.copyfileobj(input, output, 256)
