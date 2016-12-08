e946e1a964f3c29fa72dadb5d78f4568a02d6dd3
cponeill <caseyoneill78@hotmail.com>
2016-12-08 10:02:01 -0500

    prompt.py

---
+++ getpass/prompt.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import getpass
+
+p = getpass.getpass(prompt='What is your favorite color?')
+if p.lower() == 'blue':
+    print('Right. Off you go.')
+else:
+    print('NOPE!')
