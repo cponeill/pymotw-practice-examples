23a5fb907d23a44d4c32a258ab21347a208ff709
cponeill <caseyoneill78@hotmail.com>
2016-12-22 11:34:24 -0500

    string.py

---
+++ pickle/string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import pickle
+import pprint
+
+data = [{'a': 'A', 'b': 2, 'c': 3.0}]
+print('DATA:', end=' ')
+pprint.pprint(data)
+
+data_string = pickle.dumps(data)
+print('PICKLE: {!r}'.format(data_string))
