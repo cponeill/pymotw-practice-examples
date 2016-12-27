401bfc45a4fa125965df7a0911f711b75f5d0220
cponeill <caseyoneill78@hotmail.com>
2016-12-27 09:42:12 -0500

    chainmap_read.py

--- collections/chainmap_read.py
+++ collections/chainmap_read.py
@@ -3,3 +3,23 @@
 import collections
 
 a = {'a': 'A', 'c': 'C'}
+b = {'b': 'B', 'c': 'D'}
+
+m = collections.ChainMap(a, b)
+
+print('Individual Values\n')
+print('a={}'.format(m['a']))
+print('b={}'.format(m['b']))
+print('c={}'.format(m['c']))
+print()
+
+print('Keys = {}'.format(list(m.keys())))
+print('Values = {}'.format(list(m.values())))
+print()
+
+print('Items:')
+for k, v in m.items():
+    print('{} = {}'.format(k, v))
+print()
+
+print('"d" in m: {}'.format(('d' in m)))
