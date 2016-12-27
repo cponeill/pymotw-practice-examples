82c93540d2a2726dae88be198bafa88351a1f40d
cponeill <caseyoneill78@hotmail.com>
2016-12-27 09:55:29 -0500

    update_directly.py

---
+++ collections/update_directly.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import collections
+
+a = {'a': 'A', 'c': 'C'}
+b = {'b': 'B', 'c': 'D'}
+
+m = collections.ChainMap(a, b)
+print('Before:', m)
+m['c'] = 'E'
+print('After :', m)
+print('a:', a)
