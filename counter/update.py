f9e86839f5359e95e85136034aad80df10e97b66
cponeill <caseyoneill78@hotmail.com>
2016-12-28 09:47:50 -0500

    update.py

---
+++ counter/update.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import collections
+
+c = collections.Counter()
+print('Initial :', c)
+
+c.update('abcdaab')
+print('Sequence :', c)
+
+c.update({'a': 1, 'd': 5})
+print('Dict   :', c)
