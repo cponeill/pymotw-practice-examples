d68ba67bb15658688ab1fc2120d3f223d838b0b6
cponeill <caseyoneill78@hotmail.com>
2017-01-03 10:49:19 -0500

    string.py

---
+++ array/string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import array
+import binascii
+
+s = b'This is the array'
+a = array.array('b', s)
+
+print('As byte string:', s)
+print('As array      :', a)
+print('As hex        :', binascii.hexlify(a))
