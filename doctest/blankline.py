d5e469a7e2d4e59980b7116669eca3d162c209a7
cponeill <caseyoneill78@hotmail.com>
2017-01-08 11:29:05 -0500

    blankline.py

---
+++ doctest/blankline.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/env python3
+
+def double_space(lines):
+    """Prints a list of lines double-spaced.
+
+    >>> double_space(['Line one.', 'Line two.'])
+    Line one.
+    <BLANKLINE>
+    Line two.
+    <BLANKLINE>
+    """
+    for l in lines:
+        print(l)
+        print()
