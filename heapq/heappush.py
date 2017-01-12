936dee341a7cb0ef7ea89774bf4e42ce31059499
cponeill <caseyoneill78@hotmail.com>
2017-01-12 10:20:13 -0500

    heappush.py

---
+++ heapq/heappush.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import heapq
+from showtree import show_tree
+from heapdata import data
+
+heap = []
+print('random :', data)
+print()
+
+for n in data:
+    print('add {:>3}:'.format(n))
+    heapq.heappush(heap, n)
+    show_tree(heap)
