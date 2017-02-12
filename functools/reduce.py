afccc24c9ff2a9f9636171a2d51c4cd46780cd7d
cponeill <caseyoneill78@hotmail.com>
2017-02-12 14:17:13 -0500

    reduce.py

---
+++ functools/reduce.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+# reduce.py
+
+
+import functools
+
+
+def do_reduce(a, b):
+    print('do_reduce({}, {})'.format(a, b))
+    return a + b
+
+
+data = range(1, 5)
+print(data)
+result = functools.reduce(do_reduce, data)
+print('result: {}'.format(result))
