d648ad773be3c941d51ef90a772bd1ea3ab5295c
cponeill <caseyoneill78@hotmail.com>
2017-04-14 15:47:35 -0400

    proxy.py

---
+++ weakref/proxy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# proxy.py
+
+
+import weakref
+
+
+class ExpensiveObject:
+
+    def __init__(self, name):
+        self.name = name
+
+    def __del__(self):
+        print('(Deleting {}'.format(self))
+
+
+
+obj = ExpensiveObject('My Object')
+r = weakref.ref(obj)
+p = weakref.proxy(obj)
+
+print('via obj:', obj.name)
+print('via ref:', r().name)
+print('via proxy:', p.name)
+del obj
+print('via proxy:', p.name)
