b3684671f7d07d82a783af751e0153d0e0079297
cponeill <caseyoneill78@hotmail.com>
2017-05-04 09:27:44 -0400

    learning to use urljoin.

---
+++ Internet/parse/join.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# join.py
+
+from urllib.parse import urljoin
+
+print(urljoin('http://www.example.com/path/',
+              '/subpath/file.html'))
+print(urljoin('http://www.example.com/path/',
+              'subpath/file.html'))
