25d3a91b5ccd29fd434963ff51479aed6e2e228a
cponeill <caseyoneill78@hotmail.com>
2017-05-17 12:11:08 -0400

    base32.py

---
+++ Internet/base64/base32.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# base32.py
+
+import base64
+
+original_data = b'This is the data, in the clear.'
+print('Original:', original_data)
+
+encoded_data = base64.b32encode(original_data)
+print('Encoded :', encoded_data)
+
+decoded_data = base64.b32decode(encoded_data)
+print('Decoded :', decoded_data)
