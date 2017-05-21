8c026f70950aedefc08bb24c1f5cbbc2cf951d7a
cponeill <caseyoneill78@hotmail.com>
2017-05-21 10:51:11 -0400

    b64decode.py

---
+++ Internet/base64/b64decode.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# b64decode.py
+
+import base64
+
+
+encoded_data = b'VGhpcyBpcyB0aGUgZGF0YSwgaW4gdGhlIGNsZWFyLg=='
+decoded_data = base64.b64decode(encoded_data)
+print('Encoded :', encoded_data)
+print('Decoded :', decoded_data)
