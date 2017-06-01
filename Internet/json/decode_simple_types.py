aeacc19204a7ecdc59edc0813042b2525c0b69ba
cponeill <caseyoneill78@hotmail.com>
2017-06-01 08:09:19 -0400

    decode_simple_types.py

---
+++ Internet/json/decode_simple_types.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# decode_simple_types.py
+
+import json
+
+data = [{'a': 'A', 'b': (2, 4), 'c': 3.0}]
+print('DATA    :', data)
+
+data_string = json.dumps(data)
+print('ENCODED :', data_string)
+
+decoded = json.loads(data_string)
+print('DECODED :', decoded)
+
+print('ORIGINAL:', type(data[0]['b']))
+print('DECODED :', type(decoded[0]['b']))
