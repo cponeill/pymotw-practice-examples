b143a669948a266f671f92fa4af224e34d93cfee
cponeill <caseyoneill78@hotmail.com>
2017-06-11 07:59:06 -0400

    encoder_iterable.py

---
+++ Internet/json/encoder_iterable.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# encoder_iterable.py
+
+import json
+
+encoder = json.JSONEncoder()
+data = [{'a': 'A', 'b': (2, 4), 'c': 3.0}]
+
+for part in encoder.iterencode(data):
+    print('PART:', part)
