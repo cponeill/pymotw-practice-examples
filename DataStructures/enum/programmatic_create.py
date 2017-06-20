aab3534cc7237af9d885a309fec69355eb4afc42
cponeill <caseyoneill78@hotmail.com>
2017-06-20 10:04:30 -0400

    programmatic_create.py

---
+++ DataStructures/enum/programmatic_create.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# programmatic_create.py
+
+import enum
+
+BugStatus = enum.Enum(
+    value='BugStatus',
+    names=('fix_released fix_commited in_progress '
+           'wont_fix invalid incomplete new'),
+)
+
+print('Member: {}'.format(BugStatus.new))
+
+print('\nAll Members')
+for status in BugStatus:
+    print('{:15} = {}'.format(status.name, status.value))
