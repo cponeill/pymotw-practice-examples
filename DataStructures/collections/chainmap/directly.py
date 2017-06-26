ab2284743f43a38a0e4a9ae7ff376bcf6a22700e
cponeill <caseyoneill78@hotmail.com>
2017-06-26 07:49:06 -0400

    directly.py

---
+++ DataStructures/collections/chainmap/directly.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# directly.py
+
+import collections
+
+a = {'a': 'A', 'c': 'C'}
+b = {'b': 'B', 'c': 'D'}
+
+m = collections.ChainMap(a, b)
+print('Before:', m)
+m['c'] = 'E'
+print('After :', m)
+print('a:', a)
