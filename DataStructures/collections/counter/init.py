bf2855ae3b49f01dce97629cd58ce7f00449effb
cponeill <caseyoneill78@hotmail.com>
2017-06-28 14:51:19 -0400

    init.py

---
+++ DataStructures/collections/counter/init.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# init.py
+
+import collections
+
+print(collections.Counter(['a', 'b', 'c', 'a', 'b', 'b']))
+print(collections.Counter({'a': 2, 'b': 3, 'c': 1}))
+print(collections.Counter(a=2, b=3, c=1))
+
+
