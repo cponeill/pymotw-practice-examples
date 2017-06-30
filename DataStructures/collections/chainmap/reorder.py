09305c673816c26b5d350adc8d30ee45aceb2d06
cponeill <caseyoneill78@hotmail.com>
2017-06-30 09:30:37 -0400

    reorder.py

---
+++ DataStructures/collections/chainmap/reorder.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# reorder.py
+
+import collections
+
+a = {'a': 'A', 'c': 'C'}
+b = {'b': 'B', 'c': 'D'}
+
+m = collections.ChainMap(a, b)
+
+print(m.maps)
+print('c = {}\n'.format(m['c']))
+
+# reverse the list
+m.maps = list(reversed(m.maps))
+
+print(m.maps)
+print('c = {}'.format(m['c']))
