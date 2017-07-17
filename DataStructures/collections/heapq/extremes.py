f2ae6f6b06a3360502a20cc2d77bd4fc3bcab8cf
cponeill <caseyoneill78@hotmail.com>
2017-07-17 08:27:17 -0400

    heapreplace.py

---
+++ DataStructures/collections/heapq/extremes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# extremes.py
+
+import heapq
+from data import data
+
+
+print('all       :', data)
+print('3 largest :', heapq.nlargest(3, data))
+print('from sort :', list(reversed(sorted(data)[-3:])))
+print('3 smallest:', heapq.nsmallest(3, data))
+print('from sort :', sorted(data)[:3])
---
+++ DataStructures/collections/heapq/heapreplace.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# heapreplace.py
+
+import heapq
+from showtree import show_tree
+from data import data
+
+heapq.heapify(data)
+print('start:')
+show_tree(data)
+
+for n in [0, 13]:
+    smallest = heapq.heapreplace(data, n)
+    print('replace {:>2} with {:>2}:'.format(smallest, n))
+    show_tree(data)
