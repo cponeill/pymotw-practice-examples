b3070318f6310c82155760a70a9742c637a8daf5
cponeill <caseyoneill78@hotmail.com>
2017-08-01 09:32:22 -0400

    recursion.py

---
+++ DataStructures/pprint/recursion.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# recursion.py
+
+from pprint import pprint
+
+local_data = ['a', 'b', 1, 2]
+local_data.append(local_data)
+
+print('id(local_data) =>', id(local_data))
+pprint(local_data)
