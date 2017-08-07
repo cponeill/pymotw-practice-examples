53d44e5faa8e25f8b7de1d12c8d8ffdd116ba030
cponeill <caseyoneill78@hotmail.com>
2017-08-07 03:01:38 -0400

    perf_counter.py

---
+++ Dates/time/perf_counter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# perf_counter.py
+
+import hashlib
+import time
+
+# Data used to calculate md5 checksums
+data = open(__file__, 'rb').read()
+
+loop_start = time.perf_counter()
+
+for i in range(5):
+    iter_start = time.perf_counter()
+    h = hashlib.sha1()
+    for i in range(3000000):
+        h.update(data)
+    cksum = h.digest()
+    now = time.perf_counter()
+    loop_elapsed = now - loop_start
+    iter_elapsed = now - iter_start
+    print(time.ctime(), ': {:0.3f} {:0.3f}'.format(
+        iter_elapsed, loop_elapsed))
