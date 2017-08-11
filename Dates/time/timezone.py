3df52b3e855071ec2146c69385ffdba95469ae3d
cponeill <caseyoneill78@hotmail.com>
2017-08-11 10:09:12 -0400

    timezone.py

---
+++ Dates/time/timezone.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# timezone.py
+
+import time
+import os
+
+
+def show_zone_info():
+    print('  TZ    :', os.environ.get('TZ', '(not set)'))
+    print('  tzname:', time.tzname)
+    print('  Zone  : {} ({})'.format(
+        time.timezone, (time.timezone / 3600)))
+    print('  DST   :', time.daylight)
+    print('  Time  :', time.ctime())
+    print()
+
+
+print('Default :')
+show_zone_info()
+
+ZONES = [
+    'GMT',
+    'Europe/Amsterdam',
+]
+
+for zone in ZONES:
+    os.environ['TZ'] = zone
+    time.tzset()
+    print(zone, ':')
+    show_zone_info()
