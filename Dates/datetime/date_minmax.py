a42ad3c4efd40c9188830a12bf7838432a1ee23e
cponeill <caseyoneill78@hotmail.com>
2017-08-18 10:11:39 -0400

    date_minmax.py

---
+++ Dates/datetime/date_minmax.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# date_minmax.py
+
+import datetime
+
+print('Earliest  :', datetime.date.min)
+print('Latest    :', datetime.date.max)
+print('Resolution:', datetime.date.resolution)
