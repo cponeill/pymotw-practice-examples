75cb1cad5d62b1d5772bc058a3d3a02c55f39673
cponeill <caseyoneill78@hotmail.com>
2017-08-21 09:24:49 -0400

    fromfile.py

---
+++ Building/argparse/fromfile.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# fromfile.py
+
+import argparse
+import shlex
+
+parser = argparse.ArgumentParser(description='Short sample app',
+                                 fromfile_prefix_chars='@',
+                                 )
+
+parser.add_argument('-a', action="store_true", default=False)
+parser.add_argument('-b', action="store", dest="b")
+parser.add_argument('-c', action="store", dest="c", type=int)
+
+print(parser.parse_args(['@argparse_fromfile_prefix_chars.txt']))
