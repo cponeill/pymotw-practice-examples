a48b4acb56834f387c1ac662d34bd1ff876ea3d7
cponeill <caseyoneill78@hotmail.com>
2017-09-08 09:46:13 -0400

    getpwname.py

---
+++ unix/getpwname.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+import pwd
+import sys
+
+username = sys.argv[1]
+user_info = pwd.getpwnam(username)
+
+print('Username:', user_info.pw_name)
+print('Password:', user_info.pw_passwd)
+print('Comment :', user_info.pw_gecos)
+print('UID/GID :', user_info.pw_uid, '/', user_info.pw_gid)
+print('Home    :', user_info.pw_dir)
+print('Shell   :', user_info.pw_shell)
