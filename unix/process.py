164ff673ac9ab62d492b777980fec8951cd954a0
cponeill <caseyoneill78@hotmail.com>
2017-09-09 09:21:42 -0400

    process.py

---
+++ unix/process.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+import pwd
+import os
+
+uid = os.getuid()
+user_info = pwd.getpwuid(uid)
+print('Currently running with UID={} username={}'.format(uid, user_info.pw_name))
