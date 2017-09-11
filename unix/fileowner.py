46cd26bfec7d069a11da8acaa6cf0b9211b8fbf2
cponeill <caseyoneill78@hotmail.com>
2017-09-11 10:58:15 -0400

    fileowner.py

--- unix/fileowner.py
+++ unix/fileowner.py
@@ -4,8 +4,10 @@
 import pwd
 import os
 
+files = 'file.txt'
 filename = 'fileowner.py'
 stat_info = os.stat(filename)
 owner = pwd.getpwuid(stat_info.st_uid).pw_name
 
 print('{} is owned by {} ({})'.format(filename, stat_info, owner))
+print('The name of the file is {}'.format(files))
